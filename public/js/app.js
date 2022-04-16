d3c10b66ee077344c31936192466a9128445397d
unknown <Bawiani>
2022-04-16 12:24:51 +0000

    Remove unneccessary items

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,5 @@
 console.log('This is a javascript file!');
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     });
-// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
